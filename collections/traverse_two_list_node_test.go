12b0ef3dfbfce9c83c59ff24e25f013097fe80c4
吴翱翔 <pylint@yandex.com>
2020-05-18 17:41:24 +0800

    解决了第二题(链表遍历问题)

--- collections/traverse_two_list_node_test.go
+++ collections/traverse_two_list_node_test.go
@@ -14,7 +14,7 @@ type ListNode struct {
 func sliceToListNode(numbers []int) *ListNode {
 	dummyHead := new(ListNode)
 	current := dummyHead
-	for i:=0; i< len(numbers); i++ {
+	for i := 0; i < len(numbers); i++ {
 		current.Next = new(ListNode)
 		current = current.Next
 		current.Val = numbers[i]
@@ -35,6 +35,63 @@ func listNodeToSlice(listNode *ListNode) []int {
 func TestHelperMethods(t *testing.T) {
 	result := sliceToListNode([]int{1, 2, 3})
 	if !reflect.DeepEqual(listNodeToSlice(result), []int{1, 2, 3}) {
+		t.Errorf("Wrong Answer")
+	}
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	dummyHead := new(ListNode)
+	current := dummyHead
+	sumOrCarry := 0
+	for {
+		if l1 != nil && l2 != nil {
+			current.Next = new(ListNode)
+			current = current.Next
+			sumOrCarry = sumOrCarry + l1.Val + l2.Val
+			current.Val = sumOrCarry % 10
+			sumOrCarry /= 10
+			l1, l2 = l1.Next, l2.Next
+		} else if l1 != nil && l2 == nil {
+			current.Next = new(ListNode)
+			current = current.Next
+			sumOrCarry = sumOrCarry + l1.Val
+			current.Val = sumOrCarry % 10
+			sumOrCarry /= 10
+			l1 = l1.Next
+		} else if l1 == nil && l2 != nil {
+			current.Next = new(ListNode)
+			current = current.Next
+			sumOrCarry = sumOrCarry + l2.Val
+			current.Val = sumOrCarry % 10
+			sumOrCarry /= 10
+			l2 = l2.Next
+		} else {
+			break
+		}
+	}
+	// 如果还有进位，应对testCase: [5],[5] => [0, 1]
+	if sumOrCarry > 0 {
+		current.Next = new(ListNode)
+		current = current.Next
+		current.Val = sumOrCarry
+	}
+	return dummyHead.Next
+}
+
+func TestAddTwoNumbersTestCase2(t *testing.T) {
+	ln1 := sliceToListNode([]int{5})
+	ln2 := sliceToListNode([]int{5})
+	result := addTwoNumbers(ln1, ln2)
+	if !reflect.DeepEqual(listNodeToSlice(result), []int{0, 1}) {
+		t.Errorf("Wrong Answer")
+	}
+}
+
+func TestAddTwoNumbersTestCase1(t *testing.T) {
+	ln1 := sliceToListNode([]int{2, 4, 3})
+	ln2 := sliceToListNode([]int{5, 6, 4})
+	result := addTwoNumbers(ln1, ln2)
+	if !reflect.DeepEqual(listNodeToSlice(result), []int{7, 0, 8}) {
 		t.Errorf("Wrong Answer")
 	}
 }
