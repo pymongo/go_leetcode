0fe0a426c267f18d798b57b3362ce398c0408ce1
吴翱翔 <pylint@yandex.com>
2020-05-28 16:43:13 +0800

    88题添加了最佳答案

--- sorting/merge_two_sorted_array_test.go
+++ sorting/merge_two_sorted_array_test.go
@@ -18,44 +18,35 @@ var testCases = []TestCase{
 	{Nums1: []int{1, 2, 3, 0, 0, 0}, M: 3, Nums2: []int{2, 5, 6}, N: 3, Result: []int{1, 2, 2, 3, 5, 6}},
 	{[]int{1}, 1, []int{}, 0, []int{1}},
 	{[]int{0}, 0, []int{1}, 1, []int{1}},
-	{[]int{2.0}, 1, []int{1}, 1, []int{1,2}},
+	{[]int{2,0}, 1, []int{1}, 1, []int{1,2}},
 }
 
-/*
-Input:    [2,0] 1 [1] 1
-Output:   [2,2]
-Expected: [1,2]
-*/
 func merge(nums1 []int, m int, nums2 []int, n int) {
-	// Input: [1], 1, [ ], 0
-	if len(nums2) == 0 {
-		return
-	}
-	// Input: [0], 0, [1], 1
-	if m == 0 {
-		for i:=0; i< len(nums2); i++ {
-			nums1[i] = nums2[i]
-		}
-		return
-	}
-	nums1ReadIdx := m - 1
-	nums2ReadIdx := n - 1
+	nums1ReadIndex := m - 1
+	nums2ReadIndex := n - 1
 	nums1WriteIdx := m + n - 1
-	for nums1ReadIdx >= 0 && nums2ReadIdx >= 0 {
-		if nums1[nums1ReadIdx] > nums2[nums2ReadIdx] {
-			nums1[nums1WriteIdx] = nums1[nums1ReadIdx]
-			nums1ReadIdx--
+	for nums1ReadIndex >= 0 && nums2ReadIndex >= 0 {
+		if nums1[nums1ReadIndex] > nums2[nums2ReadIndex] {
+			nums1[nums1WriteIdx] = nums1[nums1ReadIndex]
+			nums1ReadIndex--
 			nums1WriteIdx--
 		} else {
-			nums1[nums1WriteIdx] = nums2[nums2ReadIdx]
-			if nums2ReadIdx == 0 {
-				break
-			}
-			nums2ReadIdx--
+			nums1[nums1WriteIdx] = nums2[nums2ReadIndex]
+			nums2ReadIndex--
 			nums1WriteIdx--
 		}
 	}
-	// TODO add missing elements from nums2
+	/*
+		Input:    [2,0] 1 [1] 1
+		Output:   [2,2]
+		Expected: [1,2]
+	*/
+	// 如果nums1从后往前遍历完的时候，nums2还有剩余项
+	// 则把nums2[0:nums2ReadIndex]赋值/复制到nums1[0:nums2ReadIndex]中
+	// add missing elements from nums2
+	for i:=0; i<= nums2ReadIndex; i++ {
+		nums1[i] = nums2[i]
+	}
 }
 
 func TestMerge(t *testing.T) {
@@ -68,3 +59,23 @@ func TestMerge(t *testing.T) {
 		}
 	}
 }
+
+/* Best
+func merge(nums1 []int, m int, nums2 []int, n int)  {
+
+    for m > 0 && n > 0 {
+        if nums1[m - 1] > nums2[n - 1] {
+            nums1[m+n-1] = nums1[m-1]
+            m--
+        } else {
+            nums1[m+n-1] = nums2[n-1]
+            n--
+        }
+    }
+    if m == 0 {
+        for i := 0; i < n; i++ {
+            nums1[i] = nums2[i]
+        }
+    }
+}
+*/
