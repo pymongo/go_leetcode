62e81fe4475e0a78ccd0cd1440739aba0131996f
吴翱翔 <pylint@yandex.com>
2020-05-28 17:15:16 +0800

    优化了88题的变量名

--- sorting/merge_two_sorted_array_test.go
+++ sorting/merge_two_sorted_array_test.go
@@ -18,22 +18,22 @@ var testCases = []TestCase{
 	{Nums1: []int{1, 2, 3, 0, 0, 0}, M: 3, Nums2: []int{2, 5, 6}, N: 3, Result: []int{1, 2, 2, 3, 5, 6}},
 	{[]int{1}, 1, []int{}, 0, []int{1}},
 	{[]int{0}, 0, []int{1}, 1, []int{1}},
-	{[]int{2,0}, 1, []int{1}, 1, []int{1,2}},
+	{[]int{2, 0}, 1, []int{1}, 1, []int{1, 2}},
 }
 
 func merge(nums1 []int, m int, nums2 []int, n int) {
 	nums1ReadIndex := m - 1
 	nums2ReadIndex := n - 1
-	nums1WriteIdx := m + n - 1
+	nums1WriteIndex := m + n - 1
 	for nums1ReadIndex >= 0 && nums2ReadIndex >= 0 {
 		if nums1[nums1ReadIndex] > nums2[nums2ReadIndex] {
-			nums1[nums1WriteIdx] = nums1[nums1ReadIndex]
+			nums1[nums1WriteIndex] = nums1[nums1ReadIndex]
 			nums1ReadIndex--
-			nums1WriteIdx--
+			nums1WriteIndex--
 		} else {
-			nums1[nums1WriteIdx] = nums2[nums2ReadIndex]
+			nums1[nums1WriteIndex] = nums2[nums2ReadIndex]
 			nums2ReadIndex--
-			nums1WriteIdx--
+			nums1WriteIndex--
 		}
 	}
 	/*
@@ -44,8 +44,10 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 	// 如果nums1从后往前遍历完的时候，nums2还有剩余项
 	// 则把nums2[0:nums2ReadIndex]赋值/复制到nums1[0:nums2ReadIndex]中
 	// add missing elements from nums2
-	for i:=0; i<= nums2ReadIndex; i++ {
-		nums1[i] = nums2[i]
+	if nums2ReadIndex > -1 {
+		for i := 0; i <= nums2ReadIndex; i++ {
+			nums1[i] = nums2[i]
+		}
 	}
 }
 
