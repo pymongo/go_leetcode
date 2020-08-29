d849e1317b38223e518c26664b421f2c0e923d6f
吴翱翔 <pylint@yandex.com>
2020-08-29 13:34:02 +0800

    AC了转置矩阵一题

---
+++ easy/test_transpose_matrix.go
@@ -0,0 +1,28 @@
+package easy
+
+func transpose(A [][]int) [][]int {
+	m, n := len(A), len(A[0])
+	res := make([][]int, 0, n)
+	for j := 0; j < n; j++ {
+		row := make([]int, 0, m)
+		for i := 0; i < m; i++ {
+			row = append(row, A[i][j])
+		}
+		res = append(res, row)
+	}
+	return res
+}
+
+func transpose2(A [][]int) [][]int {
+	m, n := len(A), len(A[0])
+	res := make([][]int, len(A[0]))
+	for i := range res {
+		res[i] = make([]int, m)
+	}
+	for row := 0; row < m; row++ {
+		for col := 0; col < n; col++ {
+			res[col][row] = A[row][col]
+		}
+	}
+	return res
+}
